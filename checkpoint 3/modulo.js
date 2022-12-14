5fe867299fd89c83e9d6f7a12117d05f8016bb89
WAGNER YURI <43749317+wys16@users.noreply.github.com>
2022-12-14 19:28:52 -0300

    programacao imperativa

---
+++ checkpoint 3/modulo.js
@@ -0,0 +1,11 @@
+listaEstudantes = [
+  { nome: "marcelo rodrigues", qtdFaltas: 1, notas: [10, 5, 10] },
+  { nome: "marcus rodrigues", qtdFaltas: 1, notas: [10, 7, 9] },
+  { nome: "elton neves", qtdFaltas: 2, notas: [8.7, 8.5, 8.8] },
+  { nome: "julio praxedes", qtdFaltas: 2, notas: [8.7, 8.8, 8.8] },
+  { nome: "moises bollela", qtdFaltas: 2, notas: [9, 8.8, 9] },
+  { nome: "pedro siqueira", qtdFaltas: 2, notas: [8.7, 8, 8.8] }
+];
+
+
+module.exports = listaEstudantes;
